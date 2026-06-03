314c0e10f97ebb71583e4b01e1177c00c7c1afb9
Ahmed <gamerahmed933@gmail.com>
2026-06-03 17:55:48 +0300

    Fix Database connection

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,16 @@ import { Pool } from "pg";
 import { config } from "dotenv";
 config();
 const pool = new Pool({
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    connectionString: process.env.DATABASE_URL,
+    user: process.env.DATABASE_URL ? undefined : process.env.DB_USER,
+    port: process.env.DATABASE_URL ? undefined : process.env.DB_PORT,
+    host: process.env.DATABASE_URL ? undefined : process.env.DB_HOST,
+    password: process.env.DATABASE_URL ? undefined : process.env.DB_PASSWORD,
+    database: process.env.DATABASE_URL ? undefined : process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 pool.on('connect', () => {
     console.log('Database is connected');
 
